2360539fdf6e0623fe55cefd113a5ac52bafc4fe
Andriana Popp <115152248+andrianapopp@users.noreply.github.com>
2023-04-12 18:49:42 +0300

    changed

--- scripts/app.js
+++ scripts/app.js
@@ -3,7 +3,8 @@ const num = prompt(`Your number`)
 if (num < 1 ){
     alert(NaN)
 } else {
-    for (let i = 2; i < num / 2; i++)
+    for (let i = 2; i < num / 2; i++) {
         alert(i)
+    }
 }
 alert(num)
